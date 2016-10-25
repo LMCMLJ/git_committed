cf636e48d7dc6a08f934443d649b2da6e5b6fd1f
LMCMLJ <mjcalvey2@gmail.com>
2016-10-25 17:15:17 +0100

    Adds cron

--- git_committed.sh
+++ git_committed.sh
@@ -3,6 +3,7 @@
 # Written by Michael Calvey
 
 cd '/Users/michaelcalvey/Programming/projects/Bash/git_committed/'
-"cron" >> cronfile.txt
+text="cron"
+text >> cronfile.txt
 git add-commit -m "Adds cron"
 git push origin master
